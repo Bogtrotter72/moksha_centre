e1cb9810f1099b06b975adfb113387110f620b56
Ciaran McCloskey <2ciaran7@gmail.com>
2020-08-17 18:05:53 +0100

    added product list / detail views & templates

--- moksha_centre/products/views.py
+++ moksha_centre/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
