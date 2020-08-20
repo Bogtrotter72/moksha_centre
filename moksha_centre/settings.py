b755d6b6a1d1459f13188e9c95b9ba20a9e3714d
Ciaran McCloskey <2ciaran7@gmail.com>
2020-08-20 13:21:09 +0100

    added dj_database_url & psycopg2-binary

--- moksha_centre/settings.py
+++ moksha_centre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,11 +109,15 @@ WSGI_APPLICATION = 'moksha_centre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('DATABASE_URL')
 }
 
 
