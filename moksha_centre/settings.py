2348e99754e88a690127bae1ff647157277f35d6
Ciaran McCloskey <2ciaran7@gmail.com>
2020-08-21 15:14:43 +0100

    updated settings.py

--- moksha_centre/settings.py
+++ moksha_centre/settings.py
@@ -113,16 +113,17 @@ WSGI_APPLICATION = 'moksha_centre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'USE_POSTGRES' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
