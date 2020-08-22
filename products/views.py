cd5e0faac3baab67718eb039573ecad73675d18b
Ciaran McCloskey <2ciaran7@gmail.com>
2020-08-23 00:05:50 +0100

    added product search and filtering

--- products/views.py
+++ products/views.py
@@ -1,10 +1,40 @@
+from django.contrib import messages
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render, reverse
 from .models import CarouselImage, Product
 
 
 def all_products(request):
     object_list = Product.objects.all()
+    filtered_list = []
+    query = None
+    tag = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter a search term!")
+            return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        object_list = object_list.filter(queries)
+
+    if 'tag' in request.GET:
+        tags = request.GET['tag'].split(',')
+        tags.sort()
+
+
+        for product in object_list:
+            product_tags = []
+            for tag in product.tags.all():
+                product_tags.append(tag.name)
+            product_tags.sort()
+            if tags == product_tags:
+                filtered_list.append(product)
+                product_tags = []
+
+        object_list = filtered_list
 
     # limit products to 20 per page
     paginator = Paginator(object_list, 20)
@@ -19,6 +49,7 @@ def all_products(request):
     context = {
         'page': page,
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
