347e023e315a8c2b7f93cc17f2fed5a78eed08a9
Ciaran McCloskey <2ciaran7@gmail.com>
2020-08-25 12:32:54 +0100

    added product increment/decrement/update/remove logic

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import add_to_bag, view_bag
+from .views import add_to_bag, adjust_bag, remove_from_bag, view_bag
 
 urlpatterns = [
     path('', view_bag, name="view_bag"),
     path('add/<item_id>', add_to_bag, name="add_to_bag"),
+    path('adjust/<item_id>/', adjust_bag, name='adjust_bag'),
+    path('remove/<item_id>/', remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, HttpResponse, redirect, render, reverse
+from products.models import Product
 
 # Create your views here.
 def view_bag(request):
@@ -30,5 +32,57 @@ def add_to_bag(request, item_id):
             bag[item_id] = quantity
     
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:                                                                # If the item has a size
+        if quantity > 0:                                                    # and the quantity is greater than 0
+            bag[item_id]['items_by_size'][size] = quantity                  # get the specific size and set its quantity accordingly
+        else:
+            del bag[item_id]['items_by_size'][size]                         # or remove if quantity is zero
+            if not bag[item_id]['items_by_size']:                           # If items by size is now empty
+                bag.pop(item_id)                                            # remove the item entirely
+
+    else:                                                                   # If there is no size
+        if quantity > 0:                                                    # and the quantity is greater than 0
+            bag[item_id] = quantity                                         # update the quantity
+        else:
+            bag.pop(item_id)                                                # remove the item entirely
+    
+    request.session['bag'] = bag                                            # place bag into the session
+    return redirect(reverse('view_bag'))                                    # return to the shopping bag
+
+
+def remove_from_bag(request, item_id):
+    try:
+        product = Product.objects.get(pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:                                                            # If the item has a size
+            del bag[item_id]['items_by_size'][size]                         # remove the specific size key in the items_by_size dictionary
+            if not bag[item_id]['items_by_size']:                           # If items by size is now empty
+                bag.pop(item_id)                                                # remove the item entirely
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+
+        else:                                                               # If there is no size
+            bag.pop(item_id)                                                    # remove the item entirely
+            messages.success(request, f'Removed {product.name} from your bag')
+        
+        request.session['bag'] = bag                                        # place bag into the session
+        return HttpResponse(status=200)                                     # Send a Http 200 response to indicate successful removal of item
+
+    except Exception as e:
+        messages.error(request, f'Error removing item; {e}')
+        return HttpResponse(status=500)
